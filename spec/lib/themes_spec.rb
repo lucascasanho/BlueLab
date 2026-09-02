6d6d9591ee1f49d28036e78a1b77597647087b42
lucas <jensenhunterdbd@gmail.com>
2026-09-02 17:09:12 -0400

    Guard BlueLab theme display name

--- spec/lib/themes_spec.rb
+++ spec/lib/themes_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe Themes do
       expect(described_class.instance.names).to include('mastodon-bird-ui-auto', 'default')
     end
   end
+
+  describe 'BlueLab labels' do
+    it 'uses the BlueLab name in supported fallback locales' do
+      expect(I18n.t('themes.mastodon-bird-ui-auto', locale: :en)).to eq('BlueLab')
+      expect(I18n.t('themes.mastodon-bird-ui-auto', locale: :'pt-BR')).to eq('BlueLab')
+    end
+  end
 end
