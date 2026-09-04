465fa625b2d1ce211d196c4bfade8e5e5cc12a35
lucas <jensenhunterdbd@gmail.com>
2026-09-04 08:05:46 -0400

    Cover Chromium PWA manifest requirements

--- spec/serializers/manifest_serializer_spec.rb
+++ spec/serializers/manifest_serializer_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe ManifestSerializer do
       expect(serializer.prefer_related_applications).to be false
     end
   end
+
+  describe '#icons' do
+    it 'includes the icon sizes required by Chromium install promotion' do
+      sizes = serializer.icons.pluck(:sizes)
+
+      expect(sizes).to include('192x192', '512x512')
+    end
+  end
+
+  describe 'install navigation metadata' do
+    it 'opens as a standalone app within the instance scope' do
+      expect(serializer.display).to eq('standalone')
+      expect(serializer.start_url).to eq('/')
+      expect(serializer.scope).to eq('/')
+    end
+  end
 end
