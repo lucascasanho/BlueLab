edfb73cd156ad12e74aa0b11803b54a225c0cf25
lucas <jensenhunterdbd@gmail.com>
2026-09-06 02:09:48 -0400

    Enable compose custom emoji image fallback

--- app/javascript/entrypoints/application.ts
+++ app/javascript/entrypoints/application.ts
@@ -1,11 +1,13 @@
 import { loadLocale } from 'mastodon/locales';
 import main from 'mastodon/main';
 import { loadPolyfills } from 'mastodon/polyfills';
+import { initializeComposeEmojiImageFallback } from 'mastodon/utils/compose_emoji_image_fallback';
 import { initializeEmojiHoverZoom } from 'mastodon/utils/emoji_hover_zoom';
 
 loadPolyfills()
   .then(loadLocale)
   .then(() => {
+    initializeComposeEmojiImageFallback();
     initializeEmojiHoverZoom();
     return main();
   })
