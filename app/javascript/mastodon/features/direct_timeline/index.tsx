1e687d4e5da8d6a284f991f0462ea510bf1fc174
lucas <jensenhunterdbd@gmail.com>
2026-09-10 03:34:36 -0400

    Align Blue 2 message notice i18n metadata with upstream

--- app/javascript/mastodon/features/direct_timeline/index.tsx
+++ app/javascript/mastodon/features/direct_timeline/index.tsx
@@ -127,6 +127,7 @@ const DirectTimeline: React.FC<ColumnBase> = ({ columnId, multiColumn }) => {
             <FormattedMessage
               id='compose.message.notice'
               defaultMessage='Messages are not end-to-end encrypted'
+              description='Message refers to a direct message. For languages where this is confusing, "chat" or "direct message" can be used.'
             />{' '}
             <a
               href='https://docs.joinmastodon.org/user/posting/#private'
