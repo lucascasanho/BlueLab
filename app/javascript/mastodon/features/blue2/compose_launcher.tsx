719c8271385936400622153b4e36a6fd6ed21a62
lucas <jensenhunterdbd@gmail.com>
2026-09-11 16:29:50 -0400

    Use canonical compose icon in Blue 2 launcher

--- app/javascript/mastodon/features/blue2/compose_launcher.tsx
+++ app/javascript/mastodon/features/blue2/compose_launcher.tsx
@@ -3,6 +3,7 @@ import { useCallback } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import { AvatarById } from '@/mastodon/components/avatar';
+import { ComposeIcon } from '@/mastodon/components/compose_icon';
 import { useIdentity } from '@/mastodon/identity_context';
 import {
   composerOriginFromElement,
@@ -11,7 +12,6 @@ import {
 import { useAppDispatch } from '@/mastodon/store';
 
 import classes from './compose_launcher.module.scss';
-import { Blue2ComposeIcon } from './icons';
 
 export const Blue2ComposeLauncher: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -38,7 +38,7 @@ export const Blue2ComposeLauncher: React.FC = () => {
           defaultMessage='What is on your mind?'
         />
       </span>
-      <Blue2ComposeIcon size={22} />
+      <ComposeIcon size={22} />
     </button>
   );
 };
