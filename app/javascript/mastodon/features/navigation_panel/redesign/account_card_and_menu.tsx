6337b03b28986e3490e3d7c7b836f5a0703d0746
lucas <jensenhunterdbd@gmail.com>
2026-09-10 20:00:09 -0400

    Document mobile drawer click propagation boundary

--- app/javascript/mastodon/features/navigation_panel/redesign/account_card_and_menu.tsx
+++ app/javascript/mastodon/features/navigation_panel/redesign/account_card_and_menu.tsx
@@ -242,6 +242,9 @@ const SlideOutAccountMenu: React.FC = () => {
           offset={8}
         >
           {({ props: floatingProps }) => (
+            // The surface only contains interactive children; this click
+            // handler is a propagation boundary, not an extra interaction.
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
             <div
               {...floatingProps}
               className={classes.slideOutMenu}
