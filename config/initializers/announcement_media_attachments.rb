8e4aa1799bc425bdf2c71ccd038ef9384e511f39
lucas <jensenhunterdbd@gmail.com>
2026-09-12 11:38:40 -0400

    Fix communication media RuboCop

--- config/initializers/announcement_media_attachments.rb
+++ config/initializers/announcement_media_attachments.rb
@@ -3,13 +3,13 @@
 Rails.application.config.to_prepare do
   MediaAttachment.belongs_to :announcement, optional: true
 
-  MediaAttachment.scope :attached, -> {
+  MediaAttachment.scope :attached, lambda {
     where.not(status_id: nil)
       .or(where.not(scheduled_status_id: nil))
       .or(where.not(announcement_id: nil))
   }
 
-  MediaAttachment.scope :unattached, -> {
+  MediaAttachment.scope :unattached, lambda {
     where(status_id: nil, scheduled_status_id: nil, announcement_id: nil)
   }
 end
