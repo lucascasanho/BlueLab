e92fff273ba988f63e94497d36d13f74379caedc
lucas <jensenhunterdbd@gmail.com>
2026-09-14 12:35:15 -0400

    revert: keep standalone share composer unchanged

--- app/javascript/mastodon/features/standalone/compose/index.jsx
+++ app/javascript/mastodon/features/standalone/compose/index.jsx
@@ -1,30 +1,15 @@
 import { AlertsController } from 'mastodon/components/alerts_controller';
 import ComposeFormContainer from 'mastodon/features/compose/containers/compose_form_container';
-import { RedesignComposeForm } from 'mastodon/features/compose/redesign';
-import { selectComposerEditor } from 'mastodon/reducers/slices/composer';
-import { useAppSelector } from 'mastodon/store';
 import LoadingBarContainer from 'mastodon/features/ui/containers/loading_bar_container';
 import ModalContainer from 'mastodon/features/ui/containers/modal_container';
 
-const Compose = () => {
-  const composerEditor = useAppSelector(selectComposerEditor);
-  const useBlueLabComposer =
-    composerEditor === 'bluelab' &&
-    typeof document !== 'undefined' &&
-    document.body.dataset.theme === 'blue-2';
-
-  return (
-    <>
-      {useBlueLabComposer ? (
-        <RedesignComposeForm autoFocus embedded redirectOnSuccess />
-      ) : (
-        <ComposeFormContainer autoFocus withoutNavigation redirectOnSuccess />
-      )}
-      <AlertsController />
-      <ModalContainer />
-      <LoadingBarContainer className='loading-bar' />
-    </>
-  );
-};
+const Compose = () => (
+  <>
+    <ComposeFormContainer autoFocus withoutNavigation redirectOnSuccess />
+    <AlertsController />
+    <ModalContainer />
+    <LoadingBarContainer className='loading-bar' />
+  </>
+);
 
 export default Compose;
