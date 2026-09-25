87e25127931cc1a6b57909eea64ad126a8479e82
lucas <jensenhunterdbd@gmail.com>
2026-09-25 14:53:25 -0400

    feat: define user-selectable BlueLab themes

--- app/lib/themes.rb
+++ app/lib/themes.rb
@@ -8,11 +8,17 @@ class Themes
     light: '#ffffff',
   }.freeze
 
+  USER_SELECTABLE_NAMES = %w[blue-2 mastodon-bird-ui-auto mastodon-5].freeze
+
   def initialize
     @conf = YAML.load_file(Rails.root.join('config', 'themes.yml'))
   end
 
   def names
     @conf.keys
+  end
+
+  def selectable_names
+    names & USER_SELECTABLE_NAMES
   end
 end
