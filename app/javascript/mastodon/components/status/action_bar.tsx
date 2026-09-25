79c8fe72e2c2414246180797bbc31a49541731c4
lucas <jensenhunterdbd@gmail.com>
2026-09-24 20:27:31 -0400

    fix: animate favourite on stable icon wrapper

--- app/javascript/mastodon/components/status/action_bar.tsx
+++ app/javascript/mastodon/components/status/action_bar.tsx
@@ -16,8 +16,8 @@ import {
 } from '@phosphor-icons/react';
 
 import { statusInteraction } from '@/mastodon/actions/interactions';
-import { animateFavouriteIcon } from '@/mastodon/components/status/favourite_animation';
 import { fetchStatus } from '@/mastodon/actions/statuses';
+import { animateFavouriteIcon } from '@/mastodon/components/status/favourite_animation';
 import { useCurrentAccountId } from '@/mastodon/hooks/useAccountId';
 import { useAccountStatus } from '@/mastodon/hooks/useStatus';
 import { quickBoosting } from '@/mastodon/initial_state';
@@ -160,9 +160,9 @@ export const StatusActionBar: React.FC<StatusActionBarProps> = ({
         size='sm'
         variant='ghost'
         active={status.favourited}
-        animate
         title={favouriteTitle}
         leadingIcon={favouriteIcon}
+        leadingIconWrapperClassName='favourite-animation-target'
         onClick={handleFavouriteClick}
         className={classNames(
           'favourite-spin',
