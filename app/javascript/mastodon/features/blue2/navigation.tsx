23289ff1a11e822b9ed531f6ca3cfce17060f49c
lucas <jensenhunterdbd@gmail.com>
2026-09-25 15:22:03 -0400

    style: clean BlueLab navigation imports

--- app/javascript/mastodon/features/blue2/navigation.tsx
+++ app/javascript/mastodon/features/blue2/navigation.tsx
@@ -7,7 +7,6 @@ import { NavLink } from 'react-router-dom';
 
 import { CaretRightIcon, HouseIcon, StackIcon, StarIcon } from '@phosphor-icons/react';
 
-
 import { blue2Text } from '@/bluelab/i18n/blue2';
 import { useAccount } from '@/mastodon/hooks/useAccount';
 import { useIdentity } from '@/mastodon/identity_context';
