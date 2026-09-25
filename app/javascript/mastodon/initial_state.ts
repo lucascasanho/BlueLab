69b2b8862fc7e04b3368c545ebc05b6db68bcd6e
lucas <jensenhunterdbd@gmail.com>
2026-09-25 19:59:52 -0400

    feat: expose persistent account switcher credential to web app

--- app/javascript/mastodon/initial_state.ts
+++ app/javascript/mastodon/initial_state.ts
@@ -9,6 +9,7 @@ export type InitialStateLanguage = [
 
 interface InitialStateMeta {
   access_token: string;
+  account_switcher_token?: string;
   advanced_layout?: boolean;
   auto_play_gif: boolean;
   activity_api_enabled: boolean;
@@ -186,4 +187,8 @@ export const languages = initialState?.languages.map((lang) => {
 
 export function getAccessToken(): string | undefined {
   return getMeta('access_token');
+}
+
+export function getAccountSwitcherToken(): string | undefined {
+  return getMeta('account_switcher_token');
 }
