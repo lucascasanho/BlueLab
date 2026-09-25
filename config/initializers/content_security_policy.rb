040c87af0b28b650c2ea4a10ff9ceeb17f82486b
lucas <jensenhunterdbd@gmail.com>
2026-09-25 17:18:33 -0400

    fix: allow KLIPY in browser content security policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -17,8 +17,8 @@ Rails.application.config.content_security_policy do |p|
   p.default_src     :none
   p.frame_ancestors :none
   p.font_src        :self, assets_host
-  p.img_src         :self, :data, :blob, *media_hosts
-  p.media_src       :self, :data, *media_hosts
+  p.img_src         :self, :data, :blob, *media_hosts, *klipy_media_hosts
+  p.media_src       :self, :data, *media_hosts, *klipy_media_hosts
   p.manifest_src    :self, assets_host
 
   if policy.sso_host.present?
@@ -30,7 +30,7 @@ Rails.application.config.content_security_policy do |p|
   p.child_src  :self, :blob, assets_host
   p.worker_src :self, :blob, assets_host
 
-  p.connect_src :self, :data, :blob, *media_hosts, Rails.configuration.x.streaming_api_base_url
+  p.connect_src :self, :data, :blob, *media_hosts, klipy_api_host, Rails.configuration.x.streaming_api_base_url
   p.script_src  :self, assets_host, "'wasm-unsafe-eval'"
   p.frame_src   :self, :https
   p.style_src   :self, assets_host
