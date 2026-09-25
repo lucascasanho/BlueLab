8a80d1c48943f6529e6d8e9f5559b73a63ca2424
lucas <jensenhunterdbd@gmail.com>
2026-09-25 19:15:08 -0400

    feat: forward composer publish success callback

--- app/javascript/mastodon/reducers/slices/composer.ts
+++ app/javascript/mastodon/reducers/slices/composer.ts
@@ -351,7 +351,13 @@ export const selectIsMinimized = createAppSelector(
 
 export const submitComposer = createAppThunk(
   (
-    { redirectOnSuccess }: { redirectOnSuccess?: boolean },
+    {
+      redirectOnSuccess,
+      onSuccess,
+    }: {
+      redirectOnSuccess?: boolean;
+      onSuccess?: (status: ApiStatusJSON) => void;
+    },
     { getState, dispatch },
   ) => {
     const textareaValue = getComposerTextarea()?.value;
@@ -409,6 +415,8 @@ export const submitComposer = createAppThunk(
     } else {
       dispatch(
         submitCompose((status: ApiStatusJSON) => {
+          onSuccess?.(status);
+
           if (redirectOnSuccess) {
             window.location.assign(status.url);
           }
