5c6bb6830529ec71b6eea5aba0a71219e695828f
lucas <jensenhunterdbd@gmail.com>
2026-09-25 15:21:42 -0400

    fix: refine BlueLab compact and mobile navigation

--- app/javascript/mastodon/features/blue2/navigation.tsx
+++ app/javascript/mastodon/features/blue2/navigation.tsx
@@ -146,6 +146,12 @@ export const Blue2Navigation: React.FC<{
       {signedIn && <Blue2AccountMenu compact={compact && !expanded} />}
 
       <div className={classes.items}>
+        {compact && (
+          <Item to='/home' icon={HouseIcon} exact>
+            <FormattedMessage id='tabs_bar.home' defaultMessage='Home' />
+          </Item>
+        )}
+
         <Item to='/explore' icon={Blue2SearchIcon}>
           <FormattedMessage id='tabs_bar.explore' defaultMessage='Explore' />
         </Item>
