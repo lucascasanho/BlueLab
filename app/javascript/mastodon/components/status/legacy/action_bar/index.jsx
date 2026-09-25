5ffb292a4a45f30fa417a08c8b4a79278a15e024
lucas <jensenhunterdbd@gmail.com>
2026-09-24 20:27:34 -0400

    fix: target stable favourite icon wrapper

--- app/javascript/mastodon/components/status/legacy/action_bar/index.jsx
+++ app/javascript/mastodon/components/status/legacy/action_bar/index.jsx
@@ -421,7 +421,7 @@ class StatusActionBar extends ImmutablePureComponent {
           <BoostButton statusId={status.get('id')} counters={withCounters} />
         </div>
         <div className='status__action-bar__button-wrapper'>
-          <IconButton className='status__action-bar__button star-icon' active={status.get('favourited')} title={favouriteTitle} icon='star' iconComponent={status.get('favourited') ? StarIcon : StarBorderIcon} onClick={this.handleFavouriteClick} counter={withCounters ? status.get('favourites_count') : undefined} />
+          <IconButton iconWrapperClassName='favourite-animation-target' className='status__action-bar__button star-icon' active={status.get('favourited')} title={favouriteTitle} icon='star' iconComponent={status.get('favourited') ? StarIcon : StarBorderIcon} onClick={this.handleFavouriteClick} counter={withCounters ? status.get('favourites_count') : undefined} />
         </div>
         <div className='status__action-bar__button-wrapper'>
           <IconButton className='status__action-bar__button bookmark-icon' disabled={!signedIn} active={status.get('bookmarked')} title={bookmarkTitle} icon='bookmark' iconComponent={status.get('bookmarked') ? BookmarkIcon : BookmarkBorderIcon} onClick={this.handleBookmarkClick} />
