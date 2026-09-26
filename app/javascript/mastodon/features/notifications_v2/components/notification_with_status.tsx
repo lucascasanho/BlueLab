241c8a7d6ef23724e5961f82d4804f21a9444197
lucas <jensenhunterdbd@gmail.com>
2026-09-25 21:41:32 -0400

    fix: scope direct notification routing to Blue2

--- app/javascript/mastodon/features/notifications_v2/components/notification_with_status.tsx
+++ app/javascript/mastodon/features/notifications_v2/components/notification_with_status.tsx
@@ -65,7 +65,11 @@ export const NotificationWithStatus: React.FC<{
   const handlers = useMemo(
     () => ({
       open: () => {
-        if (isPrivateMention) {
+        const isBlue2 =
+          typeof document !== 'undefined' &&
+          document.body.dataset.theme === 'blue-2';
+
+        if (isBlue2 && isPrivateMention) {
           dispatch(openConversationForStatus(statusId));
         } else {
           dispatch(navigateToStatus(statusId));
