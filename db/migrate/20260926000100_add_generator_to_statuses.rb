32df8f1a5f7755e47d797d9f164378eb5eab0619
lucas <jensenhunterdbd@gmail.com>
2026-09-26 01:22:05 -0400

    fediverse: allow long generator URLs

--- db/migrate/20260926000100_add_generator_to_statuses.rb
+++ db/migrate/20260926000100_add_generator_to_statuses.rb
@@ -1,6 +1,6 @@
 class AddGeneratorToStatuses < ActiveRecord::Migration[8.1]
   def change
     add_column :statuses, :generator_name, :string
-    add_column :statuses, :generator_url, :string
+    add_column :statuses, :generator_url, :text
   end
 end
