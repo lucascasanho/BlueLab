709aa699ccb740d36cece05f25465d822c3d9796
lucas <jensenhunterdbd@gmail.com>
2026-09-25 20:34:28 -0400

    style: format public bug report controller

--- app/controllers/bug_reports_controller.rb
+++ app/controllers/bug_reports_controller.rb
@@ -2,6 +2,7 @@
 
 class BugReportsController < ApplicationController
   layout 'auth'
+
   def new
     @error_page = valid_error_page(params[:error_page])
     @current_path = sanitize_path(params[:current_path]) || request.path
