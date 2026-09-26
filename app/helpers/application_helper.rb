2cac1038ecc1c5a03292af3f452ef36419154362
lucas <jensenhunterdbd@gmail.com>
2026-09-25 20:43:43 -0400

    feat: provide current session id to account switcher

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -229,6 +229,7 @@ module ApplicationHelper
       state_params[:push_subscription] = current_account.user.web_push_subscription(current_session)
       state_params[:current_account]   = current_account
       state_params[:token]             = current_session.token
+      state_params[:session_id]        = current_session.session_id
       state_params[:admin]             = Account.find_local(Setting.site_contact_username.strip.gsub(/\A@/, ''))
     end
 
