33b2fe9d6db0e7e0154d02c9764b30b79c62c2e3
lucas <jensenhunterdbd@gmail.com>
2026-09-26 11:55:15 -0400

    fix: remove duplicate BlueLab live feed declaration

--- app/javascript/mastodon/features/firehose/index.jsx
+++ app/javascript/mastodon/features/firehose/index.jsx
@@ -68,8 +68,6 @@ const Firehose = ({ feedType, multiColumn }) => {
   const { signedIn, permissions } = useIdentity();
   const isBlue2 =
     typeof document !== 'undefined' && document.body.dataset.theme === 'blue-2';
-  const isBlue2 =
-    typeof document !== 'undefined' && document.body.dataset.theme === 'blue-2';
 
   const onlyMedia = useAppSelector((state) => state.getIn(['settings', 'firehose', 'onlyMedia'], false));
   const hasUnread = useAppSelector((state) => state.getIn(['timelines', `${feedType}${onlyMedia ? ':media' : ''}`, 'unread'], 0) > 0);
