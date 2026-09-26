15b81f6f1998524710ce9c822f7a3c07863f97d6
lucas <jensenhunterdbd@gmail.com>
2026-09-25 21:36:45 -0400

    feat: expose native conversation identifier

--- app/serializers/rest/conversation_serializer.rb
+++ app/serializers/rest/conversation_serializer.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class REST::ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :unread
+  attributes :id, :unread, :conversation_id
 
   has_many :participant_accounts, key: :accounts, serializer: REST::AccountSerializer
   has_one :last_status, serializer: REST::StatusSerializer
 
   def id
     object.id.to_s
+  end
+
+  def conversation_id
+    object.conversation_id.to_s
   end
 end
