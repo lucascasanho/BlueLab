68e5b82fbe35b4f0702cddf4eb7ae11cb852de56
lucas <jensenhunterdbd@gmail.com>
2026-09-26 01:00:29 -0400

    fix account lookup route import

--- app/javascript/mastodon/hooks/useAccountId.ts
+++ app/javascript/mastodon/hooks/useAccountId.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-import { useParams } from 'react-router';
+import { useLocation, useParams } from 'react-router';
 
 import { fetchAccount, lookupAccount } from 'mastodon/actions/accounts';
 import { normalizeForLookup } from 'mastodon/reducers/accounts_map';
