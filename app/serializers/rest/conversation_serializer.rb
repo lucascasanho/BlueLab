02470c3ffe6d5b08a0138da1fdcfc29efb1988a5
lucas <jensenhunterdbd@gmail.com>
2026-09-26 01:06:18 -0400

    promote direct messages: app/serializers/rest/conversation_serializer.rb

--- app/serializers/rest/conversation_serializer.rb
+++ app/serializers/rest/conversation_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class REST::ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :unread
+  attributes :id, :unread, :conversation_id
 
   has_many :participant_accounts, key: :accounts, serializer: REST::AccountSerializer
   has_one :last_status, serializer: REST::StatusSerializer
@@ -9,4 +9,9 @@ class REST::ConversationSerializer < ActiveModel::Serializer
   def id
     object.id.to_s
   end
+
+  def conversation_id
+    object.conversation_id.to_s
+  end
+
 end
