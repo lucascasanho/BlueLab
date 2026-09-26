44d7bedd4e22d221602174c18fc4db410b7714d0
lucas <jensenhunterdbd@gmail.com>
2026-09-26 01:20:27 -0400

    fediverse: store ActivityPub generator metadata

---
+++ db/migrate/20260926000100_add_generator_to_statuses.rb
@@ -0,0 +1,6 @@
+class AddGeneratorToStatuses < ActiveRecord::Migration[8.1]
+  def change
+    add_column :statuses, :generator_name, :string
+    add_column :statuses, :generator_url, :string
+  end
+end
