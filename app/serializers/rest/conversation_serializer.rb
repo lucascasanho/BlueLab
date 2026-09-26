12e178826d130671a5be2786aad8d960465b1e86
lucas <jensenhunterdbd@gmail.com>
2026-09-25 23:45:13 -0400

    feat: include logical thread id in conversation payload

--- app/serializers/rest/conversation_serializer.rb
+++ app/serializers/rest/conversation_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class REST::ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :unread, :conversation_id
+  attributes :id, :unread, :conversation_id, :thread_id
 
   has_many :participant_accounts, key: :accounts, serializer: REST::AccountSerializer
   has_one :last_status, serializer: REST::StatusSerializer
@@ -12,5 +12,9 @@ class REST::ConversationSerializer < ActiveModel::Serializer
 
   def conversation_id
     object.conversation_id.to_s
+  end
+
+  def thread_id
+    object.thread_id
   end
 end
