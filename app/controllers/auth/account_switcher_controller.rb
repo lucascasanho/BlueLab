f8030b943216dfe42f31469422b9aa25076baf72
lucas <jensenhunterdbd@gmail.com>
2026-09-25 20:21:08 -0400

    fix: let Warden load switched account from new session cookie

--- app/controllers/auth/account_switcher_controller.rb
+++ app/controllers/auth/account_switcher_controller.rb
@@ -24,7 +24,6 @@ class Auth::AccountSwitcherController < ApplicationController
       same_site: :lax,
     }
 
-    warden.set_user(user, scope: :user)
     session.delete(:account_switcher)
 
     render json: {
