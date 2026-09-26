29ee7f66ea73a205723dcb1f3e367fe159a93c0b
lucas <jensenhunterdbd@gmail.com>
2026-09-25 20:40:06 -0400

    refactor: restore standard devise.rb flow

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -68,7 +68,7 @@ module Devise
     class SessionActivationRememberable < Authenticatable
       def valid?
         @session_cookie = nil
-        session_cookie.present? && !request.env['mastodon.account_switcher_authentication']
+        session_cookie.present?
       end
 
       def authenticate!
