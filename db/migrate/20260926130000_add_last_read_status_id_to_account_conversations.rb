b9cb23ec8124e4f0818360041ec30faa4720f845
lucas <jensenhunterdbd@gmail.com>
2026-09-26 13:35:28 -0400

    fix: allow conversation read position data migration

--- db/migrate/20260926130000_add_last_read_status_id_to_account_conversations.rb
+++ db/migrate/20260926130000_add_last_read_status_id_to_account_conversations.rb
@@ -2,11 +2,13 @@ class AddLastReadStatusIdToAccountConversations < ActiveRecord::Migration[8.1]
   def up
     add_column :account_conversations, :last_read_status_id, :bigint
 
-    execute <<~SQL.squish
-      UPDATE account_conversations
-      SET last_read_status_id = last_status_id
-      WHERE unread = FALSE
-    SQL
+    safety_assured do
+      execute <<~SQL.squish
+        UPDATE account_conversations
+        SET last_read_status_id = last_status_id
+        WHERE unread = FALSE
+      SQL
+    end
   end
 
   def down
