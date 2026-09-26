236921926bc898f9534d9fa923dc5d042b684b48
lucas <jensenhunterdbd@gmail.com>
2026-09-25 20:00:14 -0400

    fix: use persistent web credential for account switching

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -40,7 +40,7 @@ class InitialStateSerializer < ActiveModel::Serializer
                                                 Setting.hide_status_character_counter
                                               end
       store[:wrapstodon] = wrapstodon
-      store[:account_switcher_token] = account_switcher_token
+      store[:account_switcher_token] = object.current_account.user.account_switcher_token
     else
       store[:auto_play_gif] = Setting.auto_play_gif
       store[:display_media] = Setting.display_media
