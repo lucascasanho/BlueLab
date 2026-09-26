66498b436ef10c8f3b93bf8433dd485ffdcfd772
lucas <jensenhunterdbd@gmail.com>
2026-09-26 01:53:02 -0400

    fix: remove BlueLab favorites from sidebar footer

--- app/javascript/mastodon/features/navigation_panel/redesign/index.tsx
+++ app/javascript/mastodon/features/navigation_panel/redesign/index.tsx
@@ -339,16 +339,6 @@ export const RedesignNavigationPanel: React.FC<{
                   </NavigationLink>
                   <NavigationLink
                     stacked
-                    to='/favourites'
-                    iconComponent={StarIcon}
-                  >
-                    <FormattedMessage
-                      id='navigation_bar.favourites'
-                      defaultMessage='Favorites'
-                    />
-                  </NavigationLink>
-                  <NavigationLink
-                    stacked
                     to='/bookmarks'
                     iconComponent={BookmarkSimpleIcon}
                   >
@@ -385,16 +375,6 @@ export const RedesignNavigationPanel: React.FC<{
                       id='tabs_bar.messages'
                       defaultMessage='Messages'
                       description='Message refers to a direct message. For languages where this is confusing, "chat" or "direct message" can be used.'
-                    />
-                  </NavigationLink>
-                  <NavigationLink
-                    stacked
-                    to='/favourites'
-                    iconComponent={StarIcon}
-                  >
-                    <FormattedMessage
-                      id='navigation_bar.favourites'
-                      defaultMessage='Favorites'
                     />
                   </NavigationLink>
                   <NavigationLink
